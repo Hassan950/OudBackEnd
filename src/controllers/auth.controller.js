f5ebad3120ae7e86143e59802fbbe93c330335ba
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-03-10 12:05:05 +0200

    Add authenticate and authorize

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 const User = require('../models/user.model.js')
 const authService = require('../services/auth.services.js');
 const AppError = require('../utils/AppError.js');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { promisify } = require('util');
 
 /**
  * @version 1.0.0
@@ -51,4 +53,43 @@ exports.login = async (req, res, next) => {
     token: token,
     user: user
   });
+};
+
+exports.authenticate = async (req, res, next) => {
+  // getting token and check if it is there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) return next(new AppError('Please log in.', 401));
+
+  // verification token
+  const payload = await promisify(jwt.verify)(token, config.get('JWT_KEY'));
+
+  // check if user still exists
+  const user = await User.findById(payload.id);
+  if (!user)
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exists.',
+        401
+      )
+    )
+  req.user = user;
+  next();
+};
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action.', 403)
+      );
+    }
+    next();
+  };
 };
