dd03bf6e63e1fe6fca4f21f054296fdffae782a0
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-03-12 18:09:24 +0200

    Update user model with reset token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -87,7 +87,19 @@ const userSchema = mongoose.Schema({
       token: String
     },
     select: false
-  }
+  },
+  passwordChangedAt: {
+    type: Date,
+    select: false
+  },
+  passwordResetToken: {
+    type: String,
+    select: false
+  },
+  passwordResetExpires: {
+    type: Date,
+    select: false
+  },
 }, {
   toJSON: {
     virtuals: true
