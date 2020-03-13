ba94159547235e29c0e17fdac9f34f9f02f81448
Hassan <hmibrahim1999@gmail.com>
2020-03-13 21:29:15 +0200

    Revert "Merge branch 'UsersProfile-Feat' of https://github.com/Hassan950/SpotifiyBackEnd into UsersProfile-Feat"
    This reverts commit fcb676696da4da1dd6db0edb8d2347be29e0e3ed.

--- src/controllers/userMe.controller.js
+++
@@ -1,9 +0,0 @@
-const { userMeService } = require('../services');
-const AppError = require('../utils/AppError.js');
-const config = require('config');
-const httpStatus = require('http-status');
-
-exports.getProfile = async (req, res) => {
-  const profile = await userMeService.getUser(req.user._id);
-  res.status(httpStatus.OK).send(req.user);
-};
