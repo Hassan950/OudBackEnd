7d0d98424a1120b72c734cd9f5f9d540f83117bf
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-03-13 17:09:15 +0200

    Add users seeds

---
+++ src/dev-data/data/import-data.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const config = require('config');
+const { User } = require('../../models');
+
+const DB = config.get('db');
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await User.create(users, { validateBeforeSave: false });
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await User.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
