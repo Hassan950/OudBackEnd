0dbd3a27ef304e767b8d4e457e1ae41d612a28a2
AhmedMGZ <omagdy837@yahoo.com>
2020-03-16 14:57:23 +0200

    Update browse.route.js

--- src/routes/v1/browse.route.js
+++ src/routes/v1/browse.route.js
@@ -12,14 +12,11 @@ router
   .get(validate(browseValidation.getCategories),catchAsync(browseController.getCategories));
 router
   .route('/categories/:id')
-  //.get(catchAsync(authMiddleware.authenticate), catchAsync(browseController.categoryPlaylists));
-  .get(validate(browseValidation.getCategory), catchAsync(browseController.categoryPlaylists));
+  .get(catchAsync(authMiddleware.authenticate),validate(browseValidation.getCategory), catchAsync(browseController.categoryPlaylists));
 router
   .route('/categories/:id/playlists')
-  //.get(catchAsync(authMiddleware.authenticate), catchAsync(browseController.categoryPlaylists));
-  .get(validate(browseValidation.categoryPlaylist), catchAsync(browseController.categoryPlaylists));
+  .get(catchAsync(authMiddleware.authenticate),validate(browseValidation.categoryPlaylist),  catchAsync(browseController.categoryPlaylists));
 router
   .route('/new-releases')
   .get(validate(browseValidation.newRelease),catchAsync(browseController.newReleases));
-
 module.exports = router;
