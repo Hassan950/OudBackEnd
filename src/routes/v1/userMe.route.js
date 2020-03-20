9e0c9f85fe4ed5e4fca0d81b6b3837bdbac20c9f
Hassan <hmibrahim1999@gmail.com>
2020-03-20 20:44:09 +0200

    Refactor userMe routes

--- src/routes/v1/userMe.route.js
+++ src/routes/v1/userMe.route.js
@@ -5,29 +5,45 @@ const validate = require('../../middlewares/validate');
 const { authValidation, userValidation } = require('../../validations');
 const catchAsync = require('../../utils/catchAsync');
 
-
-
 const router = express.Router();
-
+router.use(catchAsync(authMiddleware.authenticate));
 
 router
   .route('/updatePassword')
-  .patch(catchAsync(authMiddleware.authenticate), validate(authValidation.updatePassword), catchAsync(authController.updatePassword));
+  .patch(
+    catchAsync(authMiddleware.authenticate),
+    validate(authValidation.updatePassword),
+    catchAsync(authController.updatePassword)
+  );
 
 router
   .route('/')
-  .get(catchAsync(authMiddleware.authenticate), catchAsync(userController.getProfile));
+  .get(
+    catchAsync(authMiddleware.authenticate),
+    catchAsync(userController.getProfile)
+  );
 
 router
   .route('/profile')
-  .put(catchAsync(authMiddleware.authenticate), validate(userValidation.editProfile), catchAsync(userController.editProfile));
+  .put(
+    catchAsync(authMiddleware.authenticate),
+    validate(userValidation.editProfile),
+    catchAsync(userController.editProfile)
+  );
 
 router
   .route('/profilePicture')
-  .patch(catchAsync(authMiddleware.authenticate), userController.uploadImages, catchAsync(userController.updateImages))
+  .patch(
+    catchAsync(authMiddleware.authenticate),
+    userController.uploadImages,
+    catchAsync(userController.updateImages)
+  );
 
 router
   .route('/verify')
-  .post(catchAsync(authMiddleware.authenticate), catchAsync(authController.requestVerify));
+  .post(
+    catchAsync(authMiddleware.authenticate),
+    catchAsync(authController.requestVerify)
+  );
 
 module.exports = router;
