17da9a7e8688881fef35852d1847a27a5ea2c8c2
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-03-23 00:31:22 +0200

    Add doc to player controller - add player is null case

--- src/controllers/player.controller.js
+++ src/controllers/player.controller.js
@@ -1,6 +1,13 @@
 const { playerService } = require('../services');
 const AppError = require('../utils/AppError.js');
 
+/**
+ * @version 1.0.0
+ * @throws AppError 500 status
+ * @author Abdelrahman Tarek
+ * @description get user`s player
+ * @summary User player
+ */
 exports.getPlayer = async (req, res, next) => {
   if (!req.user) {
     return next(new AppError('Must Authenticate user', 500));
@@ -9,6 +16,11 @@ exports.getPlayer = async (req, res, next) => {
   const id = req.user._id;
 
   const player = await playerService.getPlayer(id);
+
+  if (!player) {
+    res.status(204);
+    return res.end();
+  }
 
   res.status(200).json({
     player: player
