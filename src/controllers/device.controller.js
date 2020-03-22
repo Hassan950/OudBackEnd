590d1b4b61393539b0f1b6512e9b3841cbc8aaab
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-03-22 22:36:21 +0200

    Fix users seeds

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -1,6 +1,13 @@
 const { deviceService } = require('../services');
 const AppError = require('../utils/AppError.js');
 
+/**
+ * @version 1.0.0
+ * @throws AppError 500 status
+ * @author Abdelrahman Tarek
+ * @description get user`s available devices
+ * @summary User devices
+ */
 exports.getUserDevices = async (req, res, next) => {
   if (!req.user) {
     return next(new AppError('Must Authenticate user', 500));
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/user.model.js');
 const authService = require('./auth.service.js');
+const bcrypt = require('bcryptjs');
 
 /**
  * 
