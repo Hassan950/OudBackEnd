255ef75a5ca067330d9c5e10e92835c121194f36
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-03-23 14:27:30 +0200

    Device feat
    * Add Device Model
    
    * Add get available devices without testing
    
    * Add devices tests
    
    * Fix users seeds

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,3 +2,4 @@ exports.homeController = require('./home.controller');
 exports.authController = require('./auth.controller');
 exports.userController = require('./user.controller');
 exports.passportController = require('./passport.controller');
+exports.deviceController = require('./device.controller');
