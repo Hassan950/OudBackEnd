c4c96babcc31b01f60b964d5eaca9e5c81c82662
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-03-23 19:54:17 +0200

    Add player model mocks

---
+++ tests/utils/models/player.model.mocks.js
@@ -0,0 +1,34 @@
+const { Player } = require('../../../src/models');
+const mongoose = require('mongoose');
+const faker = require('faker');
+
+const createFakePlayer = () => {
+  const player = new Player({
+    device: mongoose.Types.ObjectId(),
+    userId: mongoose.Types.ObjectId(),
+    progressMs: faker.random.number(),
+    isPlaying: false,
+    shuffleState: false,
+    repeatState: 'off',
+    currentlyPlayingType: 'track',
+    item: mongoose.Types.ObjectId(),
+    context: {
+      type: 'album',
+      id: mongoose.Types.ObjectId()
+    }
+  });
+  return player;
+};
+
+const players = [
+  createFakePlayer(),
+  createFakePlayer(),
+  createFakePlayer()
+];
+
+
+
+module.exports = {
+  createFakePlayer,
+  players
+}
