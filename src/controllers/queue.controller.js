2f719c1085b90de97d882e541df1ea781669d450
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-04-03 00:55:28 +0200

    Add item to the queue

--- src/controllers/queue.controller.js
+++ src/controllers/queue.controller.js
@@ -73,3 +73,57 @@ exports.repeatQueue = async (req, res, next) => {
 
   res.status(204).end();
 };
+
+
+exports.addToQueue = async (req, res, next) => {
+  if (!req.user) {
+    return next(new AppError('Must Authenticate user', 500));
+  }
+
+  const { queueIndex, deviceId, trackId } = req.query;
+
+  const id = req.user._id;
+
+  const [player, queues] = await Promise.all([
+    playerService.getPlayer(id, { populate: false }),
+    userService.getUserQueues(req.user._id)
+  ]);
+
+  if (!player) {
+    return next(new AppError('Player is not found', 404));
+  }
+
+  if (!queues || !queues.length) {
+    return next(new AppError('Queue is not found', 404));
+  }
+
+  if (queueIndex && queueIndex === 1) {
+    if (queues.length < 2) {
+      return next(new AppError('No queue with queueIndex=1', 400));
+    }
+    const queues = await userService.getUserQueues(req.user._id);
+    queues.reverse();
+
+    // save user
+    req.user.queues = queues;
+    req.user.save();
+  }
+
+  if (deviceId) {
+    const device = await deviceService.getDevice(deviceId);
+    if (!device) {
+      return next(new AppError('Device is not found', 404));
+    }
+    player.device = deviceId;
+    // save device
+    player.save();
+  }
+
+  const track = await trackService.findTrack(trackId);
+
+  if (!track) {
+    return next(new AppError('Track is not found', 404));
+  }
+
+  await queueService.appendToQueue(queues[0], [track]);
+};
