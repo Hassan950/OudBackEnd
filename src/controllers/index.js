7cc51dc74dab31658575571908b4f6aef0b6c9db
AhmedMGZ7 <48853566+AhmedMGZ7@users.noreply.github.com>
2020-04-02 04:06:35 +0200

    Browse feat
    * browse tests after rebasing
    
    * some util sevice
    
    * fixes
    
    * route fixes
    
    * fixes
    
    * Update browse.route.js
    
    * tests done
    
    * Update browse.route.js
    
    * fixes and model tests
    
    * some fixes
    
    * Update browse.service.js
    
    * some fixes in service
    
    * refactoring
    
    * tests fixes
    
    * Update .gitignore
    
    * unselect playlists
    
    * tests using mockingoose
    
    * Formatting
    
    Co-authored-by: Hassan <hmibrahim1999@gmail.com>

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 exports.homeController = require('./home.controller');
+exports.browseController = require('./browse.controller');
 exports.tracksController = require('./tracks.controller');
 exports.authController = require('./auth.controller');
 exports.userController = require('./user.controller');
