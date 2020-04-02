553feaae9c182eb5ca64bccb8499f747d2706b5f
Hassan Mohamed <42610032+Hassan950@users.noreply.github.com>
2020-04-02 04:15:40 +0200

    Revert "Browse feat" (#21)
    This reverts commit 7cc51dc74dab31658575571908b4f6aef0b6c9db.

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,6 @@ const { User, userSchema } = require('./user.model');
 const { Artist, artistSchema } = require('./artist.model');
 const { Track, trackSchema } = require('./track.model');
 const { Album, albumSchema } = require('./album.model');
-const { Category , categorySchema} = require('./category.model');
-const { Playlist, playlistSchema} = require('./playlist.model');
 const { Genre, genreSchema } = require('./genre.model');
 const { Device, deviceSchema } = require('./device.model');
 
@@ -16,10 +14,6 @@ module.exports = {
   trackSchema,
   Album,
   albumSchema,
-  Category,
-  categorySchema,
-  Playlist,
-  playlistSchema,
   Genre,
   genreSchema,
   Device,
