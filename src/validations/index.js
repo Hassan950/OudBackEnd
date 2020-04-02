a539d866a9aa08dc6848aafd95c0da53fff6f106
Hassan Mohamed <42610032+Hassan950@users.noreply.github.com>
2020-04-02 04:09:03 +0200

    Revert "Browse feat"
    This reverts commit 7cc51dc74dab31658575571908b4f6aef0b6c9db.

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,5 +1,3 @@
 exports.authValidation = require('./auth.validation');
-exports.browseValidation = require('./browse.validation');
 exports.trackValidation = require('./track.validation');
 exports.userValidation = require('./user.validation');
-
