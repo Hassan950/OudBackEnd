136c37bbccdd068a49eab1dba22c57c083c740ad
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-04-04 21:06:37 +0200

    Add player seeds

--- src/dev-data/data/import-data.js
+++ src/dev-data/data/import-data.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const config = require('config');
-const { User } = require('../../models');
+const { User, Player } = require('../../models');
 
 const DB = config.get('db');
 mongoose
@@ -18,10 +18,15 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
 );
 
+const players = JSON.parse(
+  fs.readFileSync(`${__dirname}/players.json`, 'utf-8')
+);
+
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
-    await User.create(users);
+    await User.create(users, { validateBeforeSave: false });
+    await Player.create(players, { validateBeforeSave: false });
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -33,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await User.deleteMany();
+    await Player.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
