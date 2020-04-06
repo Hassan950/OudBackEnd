339a83037a1912657e1ec0493e2a7f8323c7f610
Hassan Mohamed <42610032+Hassan950@users.noreply.github.com>
2020-04-06 02:44:27 +0200

    Playlist feat (#33)
    * some endpoints in playlist
    
    * some playlists endpoints
    
    * delete endpoint
    
    * Update playlist.route.js
    
    * switching between routes
    
    * some fixes
    
    * fixed uploading images
    
    * validations and setting default image
    
    * refactoring endpoints and adding reorder endpoint
    
    * unselecting tracks
    
    * playlists tests
    
    * Update playlist.route.js
    
    * Update playlist.controller.js
    
    * Update playlist.controller.test.js
    
    * adding 2  more endpoints
    
    * control routes
    
    * fixing tests
    
    * Fix Playlist Model already declared error
    
    * Reformating
    
    * fixes told to be made by Hassan
    
    * Update playlist.route.js
    
    * lodach-move
    
    module swaps items in array in spcific position and I used it in service. Also I corrected  objectID => objectId
    
    * Update playlist.validations.js
    
    * fixes
    
    Now Im sure all is working well
    
    * cleaning
    
    * fixing some logic
    
    * Update playlist.controller.test.js
    
    * fixed getUserPlaylists
    
    * Update playlist.controller.test.js
    
    * Update playlist.service.js
    
    * Update playlist.controller.test.js
    
    * Update playlist.service.js
    
    * fixes
    
    * Update playlist.service.js
    
    * Update playlist.service.js
    
    * Reformatting
    
    * Done
    
    * Update playlist.route.js
    
    * Change const functions to module.exports
    
    * Reformatting
    
    Co-authored-by: AhmedMGZ <omagdy837@yahoo.com>
    Co-authored-by: AhmedMGZ7 <48853566+AhmedMGZ7@users.noreply.github.com>

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 exports.homeController = require('./home.controller');
+exports.playlistController = require('./playlist.controller');
 exports.browseController = require('./browse.controller');
 exports.tracksController = require('./tracks.controller');
 exports.authController = require('./auth.controller');
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,4 +1,5 @@
 exports.authValidation = require('./auth.validation');
+exports.playlistValidation = require('./playlist.validations');
 exports.browseValidation = require('./browse.validation');
 exports.trackValidation = require('./track.validation');
 exports.userValidation = require('./user.validation');
