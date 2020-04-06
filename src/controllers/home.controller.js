c1b16825fa9c2a5ea50e886ab2e225e41a3a8bb5
Hassan <hmibrahim1999@gmail.com>
2020-04-06 19:41:50 +0200

    Edit Hello Route

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 
 const getAll = catchAsync(async (req, res) => {
-	res.send('HELLOOOOOOOOO!!!!!!!!!!!!');
+	res.send('Hello, Web');
 });
 
 module.exports = {
