440d790bacac6d7afd1b90005b2176abb1eb45ff
AhmedMGZ7 <48853566+AhmedMGZ7@users.noreply.github.com>
2020-04-10 18:57:00 +0200

    Browse f doc (#52)
    * Fdoc of Browse controller,service , validation
    
    * Seeds for categories and playlists
    
    * model tests 100
    
    * removing tests
    
    * removing changes to unit tests
    
    * Revert changes to unit tests
    
    Co-authored-by: Hassan <hmibrahim1999@gmail.com>
    Co-authored-by: Hassan Mohamed <42610032+Hassan950@users.noreply.github.com>

--- src/dev-data/data/import-data.js
+++ src/dev-data/data/import-data.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const config = require('config');
-const { User, Player } = require('../../models');
+const { User, Player, Category, Playlist } = require('../../models');
 
 const DB = config.get('db');
 mongoose
@@ -22,12 +22,21 @@ const players = JSON.parse(
   fs.readFileSync(`${__dirname}/players.json`, 'utf-8')
 );
 
+const categories = JSON.parse(
+  fs.readFileSync(`${__dirname}/categories.json`, 'utf-8')
+);
+
+const playlists = JSON.parse(
+  fs.readFileSync(`${__dirname}/playlists.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await User.create(users);
-    //await Player.create(players);
+    await Player.create(players);
+    await Category.create(categories);
+    await Playlist.create(playlists);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -40,6 +49,8 @@ const deleteData = async () => {
   try {
     await User.deleteMany();
     await Player.deleteMany();
+    await Category.deleteMany();
+    await Playlist.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
