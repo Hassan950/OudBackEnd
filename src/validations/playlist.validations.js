7f4a1974b01a3c4d9bc697539670adbf63c01008
AhmedMGZ7 <48853566+AhmedMGZ7@users.noreply.github.com>
2020-04-10 15:25:00 +0200

    playlist FDoc (#47)
    * playlist FDoc
    
    * Update playlist.controller.js
    
    Co-authored-by: Hassan Mohamed <42610032+Hassan950@users.noreply.github.com>

--- src/validations/playlist.validations.js
+++ src/validations/playlist.validations.js
@@ -1,11 +1,39 @@
 const Joi = require('@hapi/joi');
 Joi.objectId = require("joi-objectid")(Joi);
 
+
+
+/**
+ * Schema that checks that the request is valid for getPlaylist endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for getPlaylist endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ */
+
 exports.getPlaylist = {
   params: Joi.object().keys({
     id: Joi.objectId()
   })
 };
+
+/**
+ * Schema that checks that the request is valid for changePlaylist endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for changePlaylist endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ * @property {object} body An object that holds parameters that are sent up from the client in the request
+ * @property {string} body.name name of the playlist
+ * @property {boolean} query.public public of playlist
+ * @property {boolean} query.collabrative collabrative of playlist
+ * @property {string} query.description description of playlist
+ */
+
 exports.changePlaylist = {
   params: Joi.object().keys({
     id: Joi.objectId()
@@ -25,17 +53,54 @@ exports.changePlaylist = {
       .default('no description for this playlist')
   })
 };
+
+/**
+ * Schema that checks that the request is valid for uploadImage endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for uploadImage endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ */
+
 exports.uploadImage = {
   params: Joi.object().keys({
     id: Joi.objectId()
   })
 };
 
+/**
+ * Schema that checks that the request is valid for getImage endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for getImage endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ */
+
+
 exports.getImage = {
   params: Joi.object().keys({
     id: Joi.objectId()
   })
 };
+
+
+/**
+ * Schema that checks that the request is valid for reorderTracks endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for reorderTracks endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ * @property {object} body An object that holds parameters that are sent up from the client in the request
+ * @property {number} body.rangeStart the beginning of tracks to be reordered
+ * @property {number} query.rangeLength number of tracks after the rangeStart to be reordered
+ * @property {number} query.insertBefore the index to put tracks before
+ */
 
 exports.reorderTracks = {
   params: Joi.object().keys({
@@ -52,6 +117,20 @@ exports.reorderTracks = {
   })
 };
 
+
+/**
+ * Schema that checks that the request is valid for getTracks endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for getTracks endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ * @property {object} query An object containing the URL query parameters
+ * @property {number} query.limit Maximum number of elements in the response
+ * @property {number} query.offset index of the first element to return
+ */
+
 exports.getTracks = {
   params: Joi.object().keys({
     id: Joi.objectId()
@@ -64,6 +143,20 @@ exports.getTracks = {
       .default(20)
   })
 };
+
+
+/**
+ * Schema that checks that the request is valid for getUserPlaylists endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for getUserPlaylists endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the user
+ * @property {object} query An object containing the URL query parameters
+ * @property {number} query.limit Maximum number of elements in the response
+ * @property {number} query.offset index of the first element to return
+ */
 
 exports.getUserPlaylists = {
   params: Joi.object().keys({
@@ -78,6 +171,18 @@ exports.getUserPlaylists = {
   })
 };
 
+/**
+ * Schema that checks that the request is valid for deleteTracks endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for deleteTracks endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ * @property {object} query An object containing the URL query parameters
+ * @property {array} query.uris array of tracks url to be deleted
+ */
+
 exports.deleteTracks = {
   params: Joi.object().keys({
     id: Joi.objectId()
@@ -86,6 +191,22 @@ exports.deleteTracks = {
     uris: Joi.array().items(Joi.string()).required()
   })
 };
+
+
+/**
+ * Schema that checks that the request is valid for createUserPlaylist endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for createUserPlaylist endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ * @property {object} body An object that holds parameters that are sent up from the client in the request
+ * @property {string} body.name name of the playlist
+ * @property {boolean} query.public public of playlist
+ * @property {boolean} query.collabrative collabrative of playlist
+ * @property {string} query.description description of playlist
+ */
 
 exports.createUserPlaylist = {
   params: Joi.object().keys({
@@ -108,6 +229,19 @@ exports.createUserPlaylist = {
   })
 };
 
+
+/**
+ * Schema that checks that the request is valid for replaceTracks endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for replaceTracks endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ * @property {object} query An object containing the URL query parameters
+ * @property {array} query.uris array of tracks url to be added instead of tracks inside playlist
+ */
+
 exports.replaceTracks = {
   params: Joi.object().keys({
     id: Joi.objectId()
@@ -116,6 +250,19 @@ exports.replaceTracks = {
     uris: Joi.array().items(Joi.string()).required()
   })
 };
+
+/**
+ * Schema that checks that the request is valid for addTracks endpoint
+ *
+ * @author Ahmed Magdy
+ * @summary Schema for a req for addTracks endpoint
+ * @namespace
+ * @property {object} params An object containing parameter values parsed from the URL path
+ * @property {string} params.id Id of the playlist
+ * @property {object} query An object containing the URL query parameters
+ * @property {array} query.uris array of tracks url to be added
+ * @property {number} query.position index which the new tracks should be added to
+ */
 
 exports.addTracks = {
   params: Joi.object().keys({
