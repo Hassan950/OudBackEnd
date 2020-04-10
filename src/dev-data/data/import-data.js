e45794e8075abfadbfbfb3bbf058dc80e0775334
mabobakr <42984614+mabobakr@users.noreply.github.com>
2020-04-10 19:44:52 +0200

    Artist func doc (#53)
    * Added functional documentation for Artist,Track,Genre and Album controllers
    
    * Completed the functional documentation of Artist and Track services
    
    * Added functional documentation for Track, Artist, Genre and Album validations
    
    * Add req res next
    
    * services func doc
    
    * Done middlewares doc
    
    * add 20 users seeds
    
    * Added seeds for genres and artists
    
    * Added seeds for albums and tracks and modified artists
    
    * Removed scripts
    
    * Added more genres
    
    Co-authored-by: fuboki10 <abdelrahman.tarek1910@gmail.com>

--- src/dev-data/data/import-data.js
+++ src/dev-data/data/import-data.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const config = require('config');
-const { User, Player, Category, Playlist } = require('../../models');
+const {
+  User,
+  Player,
+  Genre,
+  Artist,
+  Album,
+  Track,
+  Category,
+  Playlist
+} = require('../../models');
 
 const DB = config.get('db');
 mongoose
@@ -14,14 +23,20 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
-);
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 
 const players = JSON.parse(
   fs.readFileSync(`${__dirname}/players.json`, 'utf-8')
 );
 
+const genres = JSON.parse(fs.readFileSync(`${__dirname}/genres.json`, 'utf-8'));
+const artists = JSON.parse(
+  fs.readFileSync(`${__dirname}/artists.json`, 'utf-8')
+);
+
+const albums = JSON.parse(fs.readFileSync(`${__dirname}/albums.json`, 'utf-8'));
+
+const tracks = JSON.parse(fs.readFileSync(`${__dirname}/tracks.json`), 'utf-8');
 const categories = JSON.parse(
   fs.readFileSync(`${__dirname}/categories.json`, 'utf-8')
 );
@@ -34,6 +49,10 @@ const playlists = JSON.parse(
 const importData = async () => {
   try {
     await User.create(users);
+    await Genre.create(genres);
+    await Artist.create(artists);
+    await Album.create(albums);
+    await Track.create(tracks);
     await Player.create(players);
     await Category.create(categories);
     await Playlist.create(playlists);
@@ -49,6 +68,10 @@ const deleteData = async () => {
   try {
     await User.deleteMany();
     await Player.deleteMany();
+    await Genre.deleteMany();
+    await Artist.deleteMany();
+    await Album.deleteMany();
+    await Track.deleteMany();
     await Category.deleteMany();
     await Playlist.deleteMany();
     console.log('Data successfully deleted!');
