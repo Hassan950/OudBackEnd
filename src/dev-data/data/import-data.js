a60d5413ce0e4d2c241d04a9ce52e3ca110d5047
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-04-18 19:31:31 +0200

    Add seeds eminem & sadat

--- src/dev-data/data/import-data.js
+++ src/dev-data/data/import-data.js
@@ -29,9 +29,15 @@ const artists = JSON.parse(
   fs.readFileSync(`${__dirname}/artists.json`, 'utf-8')
 );
 
-const albums = JSON.parse(fs.readFileSync(`${__dirname}/albums.json`, 'utf-8'));
+const albums2 = JSON.parse(fs.readFileSync(`${__dirname}/albums.json`, 'utf-8'));
 
-const tracks = JSON.parse(fs.readFileSync(`${__dirname}/tracks.json`), 'utf-8');
+const tracks2 = JSON.parse(fs.readFileSync(`${__dirname}/tracks.json`), 'utf-8');
+
+// delete this later
+const albums = JSON.parse(fs.readFileSync(`${__dirname}/tarek_seeds/albums.json`, 'utf-8'));
+
+const tracks = JSON.parse(fs.readFileSync(`${__dirname}/tarek_seeds/tracks.json`), 'utf-8');
+
 const categories = JSON.parse(
   fs.readFileSync(`${__dirname}/categories.json`, 'utf-8')
 );
@@ -63,7 +69,9 @@ const importData = async () => {
     await Genre.create(genres);
     await Artist.create(artists);
     await Album.create(albums);
+    await Album.create(albums2);
     await Track.create(tracks);
+    await Track.create(tracks2);
     await Category.create(categories);
     await Playlist.create(playlists);
     await Queue.create(queues);
