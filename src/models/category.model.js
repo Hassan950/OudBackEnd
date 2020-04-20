4fa3d24dbc9350cd4135fbea92b3ddd48d9a2c78
Hassan <hmibrahim1999@gmail.com>
2020-04-20 13:36:48 +0200

    fix virtuals in category and playlist models

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,24 +1,34 @@
 const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'category should have a name'],
-    minlength: 3,
-    maxlength: 20,
-    trim: true,
-    unique: [true, 'this category name  already exits']
-},
-  icon: {
-    type: String,
-    match: /.(png|jpg|jpeg)$/,
-    required: [true, 'the category should have an icon'],
-    trim: true,
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'category should have a name'],
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+      unique: [true, 'this category name  already exits']
+    },
+    icon: {
+      type: String,
+      match: /.(png|jpg|jpeg)$/,
+      required: [true, 'the category should have an icon'],
+      trim: true
+    },
+    playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist' }]
   },
-  playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist' }]
-  });
+  {
+    toJSON: {
+      virtuals: true
+    },
+    toObject: {
+      virtuals: true
+    }
+  }
+);
 
-categorySchema.virtual('type').get(function () {
+categorySchema.virtual('type').get(function() {
   return 'category';
 });
 
