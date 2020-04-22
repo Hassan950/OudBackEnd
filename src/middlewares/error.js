df23af32556539e447fc2814e5dcd2c8d57e6503
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-04-22 04:03:07 +0200

    error functional documentation

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -4,6 +4,17 @@ const AppError = require('../utils/AppError');
 const config = require('config');
 const logger = require('./../config/logger');
 
+/**
+ * Convert error object to AppError Object
+ * 
+ * @function
+ * @public
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @param {Object} req Express request object
+ * @param {Object} res Express response object
+ * @param {Function} next Next Function
+ */
 const errorConverter = (err, req, res, next) => {
   let error = err;
   if (config.get('NODE_ENV') === 'development') {
@@ -11,7 +22,7 @@ const errorConverter = (err, req, res, next) => {
   }
 
   if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-  if (error.code === 'ENOENT') error = handleFileDoesNotExistError(error);
+  if (error.code === 'ENOENT') error = handleENOENTError(error);
   if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
   if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
   if (error.name === 'TokenExpiredError') error = handleJWTExpiredError(error);
@@ -26,25 +37,79 @@ const errorConverter = (err, req, res, next) => {
   next(error);
 };
 
-const handleFileDoesNotExistError = err => {
+/**
+ * Handle ENOENT Error
+ * 
+ * @function
+ * @private
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @returns {Object} AppError object
+ */
+const handleENOENTError = err => {
   return new AppError('File Does Not exist', 404);
 };
 
+/**
+ * Handle OAuth Error
+ * 
+ * @function
+ * @private
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @returns {Object} AppError object
+ */
 const handleOAuthError = err => {
   return new AppError(err.message, err.oauthError.statusCode);
 };
 
+/**
+ * Handle Cast DB Error
+ * 
+ * @function
+ * @private
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @returns {Object} AppError object
+ */
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
 
+/**
+ * Handle JWT Error
+ * 
+ * @function
+ * @private
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @returns {Object} AppError object
+ */
 const handleJWTError = err =>
   new AppError('Invalid Token. Please log in again', 401);
 
+/**
+ * Handle JWT Expired Error
+ * 
+ * @function
+ * @private
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @returns {Object} AppError object
+ */
 const handleJWTExpiredError = err =>
   new AppError('Your token has expired! Please log in again.', 401);
 
+/**
+ * Handle Validation Error
+ * 
+ * @function
+ * @private
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @returns {Object} AppError object
+ */
 const handleValidationErrorDB = err => {
   const errors = Object.values(err.errors).map(el => el.message);
 
@@ -52,6 +117,15 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+/**
+ * Handle Duplicate field value Error
+ * 
+ * @function
+ * @private
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @returns {Object} AppError object
+ */
 const handleDuplicateFieldsDB = err => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   const field = Object.keys(err.keyValue)[0];
@@ -63,6 +137,17 @@ const handleMulterError = err => {
   return new AppError(err.message, httpStatus.BAD_REQUEST)
 }
 
+/**
+ * Error handler
+ * 
+ * @function
+ * @public
+ * @author Abdelrahman Tarek
+ * @param {Object} err Error object
+ * @param {Object} req Express request object
+ * @param {Object} res Express response object
+ * @param {Function} next Next Function
+ */
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
